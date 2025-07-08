4b32ff7f25fb4333acf98b653ea0249eabeb4f79
Khakimova Rimma <46751319+elff8@users.noreply.github.com>
2025-07-08 15:37:37 +0500

    Update User.cs

--- Backend/BackEnd/Models/User.cs
+++ Backend/BackEnd/Models/User.cs
@@ -7,12 +7,12 @@ namespace BackEnd.Models
 {
 	public class User
 	{
-		public int Id { get; set; } // ID пользователя
-		public string Email { get; set; } // Электронная почта
-		public string PasswordHash { get; set; } // Хэш пароля
-		public string Salt { get; set; } // Соль для пароля
-		public string FullName { get; set; } // Полное имя пользователя 
-		public DateTime CreatedAt { get; set; } // Дата регистрации
+		public int Id { get; set; }
+		public string Email { get; set; } 
+		public string PasswordHash { get; set; } 
+		public string Salt { get; set; } 
+		public string FullName { get; set; } 
+		public DateTime CreatedAt { get; set; }
 	}
 
 
