e37a95a8f9a40ceece677fc9a8fc24cc92ed866d
Khakimova Rimma <46751319+elff8@users.noreply.github.com>
2025-07-08 15:37:13 +0500

    Update MissingWord.cs

--- Backend/BackEnd/Models/MissingWord.cs
+++ Backend/BackEnd/Models/MissingWord.cs
@@ -8,16 +8,14 @@
 		public class MissingWord
 		{
 			public int Id { get; set; }
-			public string SentenceWithBlank { get; set; }  // Предложение с пропущенным словом
-			public string CorrectWord { get; set; }  // Правильное слово
-			public int? CategoryId { get; set; }  // Категория (глагол, существительное, артикль, предлог и т.д.)
+			public string SentenceWithBlank { get; set; } 
+			public string CorrectWord { get; set; }  
+			public int? CategoryId { get; set; }  
 			public Category Category { get; set; }
-			public string Translation { get; set; }  // Перевод предложения
+			public string Translation { get; set; } 
 
-			// Внешний ключ
 			public int DifficultyLevelId { get; set; }
 
-			// Навигационное свойство
 			public DifficultyLevel DifficultyLevel { get; set; }
 
 		}
